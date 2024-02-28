17a34a6090691767d59075dc2726bb541400a648
Sujit Ghogare <116686235+itssg001@users.noreply.github.com>
2024-02-28 20:48:19 +0530

    css updated

--- js/cyber-sec.js
+++ js/cyber-sec.js
@@ -10,11 +10,14 @@ function closemenu(){
     sidemenu.style.top = "-300px"
 }
 var iframe = document.querySelector("iframe");
+var image = document.querySelector(".right-col");
 function googleform(){
    iframe.style.display = "block" 
+   if(screen.width <= 600){
+    image.style.display = "none"
+   }
 }
-// -----------------------------navbar------------------------------
-//   
+ 
 // ---------------------------Page1-------------------------
 function lodingAnimation(){
     gsap.from(".page1 .left-col h1",{
@@ -53,174 +56,13 @@ function lodingAnimation(){
 
 }
 lodingAnimation()
-// ------------------------------------Purpose and Outlook-----------------------------------------
-function lodingAnimationPage2(){
-    gsap.registerPlugin(ScrollTrigger);
-  
-    gsap.from(".head-disc h1 ,.head-disc p ,.head-disc h3  ",{
-        y:100,
-        delay:0.4,
-        opacity:0,
-        scale:0.8,
-       duration:0.5,
-        stager:0.4,
-        scrollTrigger:{
-            trigger:".head-disc h1",
-            
-        }
-
-    })
-    gsap.from(".head-disc  .card",{
-        y:100,
-        opacity:0,
-        scale:1,
-        delay:0.3,
-        duration:0.5,
-        scrollTrigger:{
-            trigger:".head-disc  .card",
-            
-        }
-    })
-    
-  
-}
-lodingAnimationPage2()
-// -----------------------------Communities------------------
-function lodaingAnimationCommunity(){
-    gsap.registerPlugin(ScrollTrigger);
-    gsap.from(".page2",{
-        y:100,
-        opacity:0,
-        delay:0,
-        duration:0.8,
-        scrollTrigger:{
-            trigger:".page2",
-           
-        }
-    })
-    gsap.from(".page2 .card1",{
-        x:-100,
-        opacity:0,
-        delay:0.6,
-        duration:0.8,
-        scrollTrigger:{
-            trigger:".page2 .card1",
-            
-        }
-    })
-    gsap.from(".page2 .card2",{
-        x:100,
-        opacity:0,
-        delay:0.6,
-        duration:0.8,
-        scrollTrigger:{
-            trigger:".page2 .card1",
-           
-        }
-    })
-    gsap.from(".page2 .card3",{
-        x:-100,
-        opacity:0,
-        delay:0.6,
-        duration:0.8,
-        scrollTrigger:{
-            trigger:".page2 .card3",
-           
-        }
-    })
-    gsap.from(".page2 .card4",{
-        x:100,
-        opacity:0,
-        delay:0,
-        duration:0.8,
-        scrollTrigger:{
-            trigger:".page2 .card4",
-            
-        }
-    })
-    gsap.from(".page2 .card5",{
-        x:-100,
-        opacity:0,
-        delay:0,
-        duration:0.8,
-        scrollTrigger:{
-            trigger:".page2 .card5",
-           
-        }
-    })
-    gsap.from(".page2 .card6",{
-        x:100,
-        opacity:0,
-        delay:0.6,
-        duration:0.8,
-        scrollTrigger:{
-            trigger:".page2 .card6",
-          
-        }
-    })
-}
-lodaingAnimationCommunity()
-// ---------------------------Join Community------------------------
-function lodaingAnimationJoinComMunity(){
-    gsap.registerPlugin(ScrollTrigger);
-    gsap.from(".join-community-container h2 , .join-community-container p, .join-community-container ul, .join-community-container button",{
-        y:100,
-        opacity:0,
-        delay:0.4,
-        duration:0.3,
-        scrollTrigger:{
-            trigger:".join-community-container h2",
-           
-           //Add 100vh to css -----** 
-        }
-    })
-
-}
-lodaingAnimationJoinComMunity()
-//---------------------------------Page3------------------------
-//-------------------------------About us----------------------------
- function lodaingAnimationAboutUs(){
-    gsap.registerPlugin(ScrollTrigger);
-    gsap.from(".about_us h2",{
-        y:100,
-        opacity:0,
-        delay:0.4,
-        duration:0.6,
-        scrollTrigger:{
-            trigger:".about_us h2",
-            
-        }
-    })
-    gsap.from(".about_us h4",{
-        y:100,
-        opacity:0,
-        delay:0.4,
-        duration:0.6,
-        scrollTrigger:{
-            trigger:".about_us h4",
-            
-            
-        }
-    })
-    gsap.from(".about_us p",{
-        y:100,
-        opacity:0,
-        delay:0.4,
-        duration:0.6,
-        scrollTrigger:{
-            trigger:".about_us p",
-            
-        }
-    })
-}
-lodaingAnimationAboutUs()
 
 function loadingAnimationFooter(){
      gsap.registerPlugin(ScrollTrigger);
      gsap.from("footer",{
         x:-1100,
-        delay:0.4,
-        duration:0.4,
+        delay:0.2,
+        duration:0.2,
         scrollTrigger:{
             trigger:"footer",
             
